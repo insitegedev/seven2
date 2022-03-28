64eec0c3659fc7fbb1b30f8675c4876ad20a8c35
Alex Zhukov <alex3dfree@gmail.com>
2022-03-28 11:59:02 +0400

    update fix category create bug

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -58,7 +58,7 @@ class CategoryController extends Controller
     public function create()
     {
         $category = $this->categoryRepository->model;
-        $categories = $this->categoryRepository->all();
+        $categories = $this->categoryRepository->getCategoryTree();
 
         //dd($categories);
 
